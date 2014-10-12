4863f3f2994d02d834b3a3c03221cfa7b48dc2b2
georgerappel <george.concei@hotmail.com>
2014-10-12 19:12:53 -0300

    Extended example.
    Added some stuff to the example for people that, just like me, needs to run anything after a set period of time,

--- src/gr/antoniom/chronometer/ChronometerExampleActivity.java
+++ src/gr/antoniom/chronometer/ChronometerExampleActivity.java
@@ -11,6 +11,18 @@ public class ChronometerExampleActivity extends Activity {
         setContentView(R.layout.main);
         
         Chronometer mChronometer = (Chronometer) findViewById(R.id.chronometer);
-        mChronometer.start();        
+        mChronometer.start();     
+        
+        //IF you want to stop your chrono after X seconds or minutes.
+        mChronometer.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
+			public void onChronometerTick(Chronometer chronometer) {
+				if (chronometer.getText().toString().equalsIgnoreCase("00:05:0")) { //When reaches 5 seconds.
+				    //Define here what happens when the Chronometer reaches the time above.
+				    chronometer.stop();
+				    Toast.makeText(getBaseContext(),"Reached the end.",
+						Toast.LENGTH_LONG).show();
+				}
+			}
+		});
     }
 }
